919e8c12a2fd58ba5c210fa3dddf683abc3ffcf6
Andrew K Kan <andrew.k.kan@gmail.com>
2023-12-04 07:02:17 -0800

    Exposing more headers

--- examples/python_bindings/clip_cpp/__init__.py
+++ examples/python_bindings/clip_cpp/__init__.py
@@ -1,1 +1,11 @@
-from .clip import Clip, ClipImageU8, ClipImageF32, ClipImageU8Batch, ClipImageF32Batch
+from clip_cpp import (
+    Clip, 
+    make_clip_image_u8, 
+    make_clip_image_f32, 
+    make_clip_image_u8_batch, 
+    make_clip_image_f32_batch,
+    ClipImageU8,
+    ClipImageF32,
+    ClipImageU8Batch,
+    ClipImageF32Batch,
+)
